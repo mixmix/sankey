8f37ef36463b52ebfaca804bd176e0c6ded975c3
mix irving <whimful@gmail.com>
2016-06-27 23:12:59 +1200

    update numbers

--- data_generator.js
+++ data_generator.js
@@ -4,9 +4,11 @@ var monthlyIncome = 80000
 
 var orgToPods = [
   { from: 'eda', to: 'teaching',     percentage: 45 },
-  { from: 'eda', to: 'relationship', percentage: 20 },
-  { from: 'eda', to: 'marketing',    percentage: 25 },
+  { from: 'eda', to: 'contingency',  percentage: 15 },
   { from: 'eda', to: 'catalyst',     percentage: 10 },
+  { from: 'eda', to: 'core',         percentage: 10 },
+  { from: 'eda', to: 'marketing',    percentage: 10 },
+  { from: 'eda', to: 'relationship', percentage: 10 },
 ]
 
 var podToEntity = [
@@ -15,6 +17,7 @@ var podToEntity = [
   { from: 'teaching', to: 'joseph', amount: 5000 },
   { from: 'teaching', to: 'don',    amount: 10000 },
   { from: 'teaching', to: 'rich',   amount: 5000 },
+  { from: 'teaching', to: 'TAs',    amount: 2500 },
 
   { from: 'relationship', to: 'jamie', amount: 5000 },
   { from: 'relationship', to: 'sarah', amount: 2500 },
@@ -23,6 +26,9 @@ var podToEntity = [
 
   { from: 'catalyst', to: 'jv',    amount: 7000 },
   { from: 'catalyst', to: 'billy', amount: 2000 },
+
+  { from: 'contingency', to: 'contingency stuff', amount: 10000 },
+  { from: 'core', to: 'core stuff',        amount: 7000 },
 ]
 
 // ------------- generats data.json -----------------------------
