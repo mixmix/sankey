6ca7dbd3dfe44b8cb1d72bb89aef076b4583047c
mix irving <whimful@gmail.com>
2016-06-28 15:36:34 +1200

    make basic interactive diagram

--- index.js
+++ index.js
@@ -2,8 +2,38 @@ var data = require('./startingData')
 var mapDataToSankeyFormat = require('./utils/mapDataToSankeyFormat')
 var render = require('./utils/render')
 
+var initialMonthlyIncome = data.monthlyIncome
+
 document.addEventListener('DOMContentLoaded', function(ev) {
-  var sankeyData = mapDataToSankeyFormat(data) 
-  render(sankeyData)
+  var input = document.getElementById('incoming-money')
+  input.value = data.monthlyIncome 
+  redraw()
+    
+  input.addEventListener('keyup', function(ev) {
+    if (isNaN(input.valueAsNumber)) input.value = initialMonthlyIncome
+    data.monthlyIncome = input.valueAsNumber
+    redraw()
+  })
+
+  document.addEventListener('keyup', function(ev) { 
+    switch (ev.keyCode) {
+      case 38 :
+        console.log('up')
+        ev.preventDefault()
+        console.log(input.valueAsNumber + 5000, input.valueAsNumber)
+        input.value = input.valueAsNumber + 5000
+      case 40 :
+        ev.preventDefault()
+        input.value = input.valueAsNumber - 5000
+    }
+  })
+  
 })
+
+var redraw = function() {
+  console.log('drawing')
+  var sankeyData = mapDataToSankeyFormat(data) 
+  document.getElementById("chart").innerHTML = ''
+  render(sankeyData)
+}
 
--- public/bundle.js
+++ public/bundle.js
@@ -3,10 +3,40 @@ var data = require('./startingData')
 var mapDataToSankeyFormat = require('./utils/mapDataToSankeyFormat')
 var render = require('./utils/render')
 
+var initialMonthlyIncome = data.monthlyIncome
+
 document.addEventListener('DOMContentLoaded', function(ev) {
-  var sankeyData = mapDataToSankeyFormat(data) 
-  render(sankeyData)
+  var input = document.getElementById('incoming-money')
+  input.value = data.monthlyIncome 
+  redraw()
+    
+  input.addEventListener('keyup', function(ev) {
+    if (isNaN(input.valueAsNumber)) input.value = initialMonthlyIncome
+    data.monthlyIncome = input.valueAsNumber
+    redraw()
+  })
+
+  document.addEventListener('keyup', function(ev) { 
+    switch (ev.keyCode) {
+      case 38 :
+        console.log('up')
+        ev.preventDefault()
+        console.log(input.valueAsNumber + 5000, input.valueAsNumber)
+        input.value = input.valueAsNumber + 5000
+      case 40 :
+        ev.preventDefault()
+        input.value = input.valueAsNumber - 5000
+    }
+  })
+  
 })
+
+var redraw = function() {
+  console.log('drawing')
+  var sankeyData = mapDataToSankeyFormat(data) 
+  document.getElementById("chart").innerHTML = ''
+  render(sankeyData)
+}
 
 
 },{"./startingData":2,"./utils/mapDataToSankeyFormat":3,"./utils/render":4}],2:[function(require,module,exports){
