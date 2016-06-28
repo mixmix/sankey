8b9302355e3494bdbe346cf54801cb2e1ac3f355
mix irving <whimful@gmail.com>
2016-06-28 18:21:16 +1200

    update to closer to actual state

--- startingData.js
+++ startingData.js
@@ -43,7 +43,9 @@ var data = {
 
     // 
 
-    { from: 'placements', to: 'relationship', amount: 10500 },
+    { from: 'placements', to: 'relationship', amount: 1000 },
+    // ideally
+    //{ from: 'placements', to: 'relationship', amount: 10500 },
   ]
 }
 
