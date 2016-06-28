9e777dfa24a687c53aeb5aeb7e6e9ca9560ca425
mix irving <whimful@gmail.com>
2016-06-28 16:27:30 +1200

    make graph interactive

--- index.js
+++ index.js
@@ -4,34 +4,41 @@ var render = require('./utils/render')
 
 var initialMonthlyIncome = data.monthlyIncome
 
-document.addEventListener('DOMContentLoaded', function(ev) {
+window.addEventListener('keydown', function(e) {
+  if (e.keyCode === 38 || e.keyCode === 40) {
+    e.preventDefault()
+  }
+})
+
+document.addEventListener('DOMContentLoaded', function(e) {
   var input = document.getElementById('incoming-money')
   input.value = data.monthlyIncome 
   redraw()
     
-  input.addEventListener('keyup', function(ev) {
+  input.addEventListener('keyup', function(e) {
     if (isNaN(input.valueAsNumber)) input.value = initialMonthlyIncome
     data.monthlyIncome = input.valueAsNumber
     redraw()
   })
 
-  document.addEventListener('keyup', function(ev) { 
-    switch (ev.keyCode) {
-      case 38 :
-        console.log('up')
-        ev.preventDefault()
-        console.log(input.valueAsNumber + 5000, input.valueAsNumber)
-        input.value = input.valueAsNumber + 5000
-      case 40 :
-        ev.preventDefault()
-        input.value = input.valueAsNumber - 5000
+  document.addEventListener('keydown', function(e) { 
+    var value    = input.valueAsNumber
+    var newValue = input.valueAsNumber
+
+    if (e.keyCode == 38) {
+      newValue += 5000
+    } else if (e.keyCode == 40) {
+      newValue -= 5000
     }
+
+    data.monthlyIncome = newValue
+    input.value = newValue
+    redraw()
   })
   
 })
 
 var redraw = function() {
-  console.log('drawing')
   var sankeyData = mapDataToSankeyFormat(data) 
   document.getElementById("chart").innerHTML = ''
   render(sankeyData)
